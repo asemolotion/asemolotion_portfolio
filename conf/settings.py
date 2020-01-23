67f31995ced60de8fadaef44e5a7c330a60ebb16
広石財 <hiroishizai@takaras-mac.local>
2020-01-23 15:44:54 +0900

    disabled futurewarning #13

--- conf/settings.py
+++ conf/settings.py
@@ -142,3 +142,10 @@ if not DEBUG:
 
     db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
     DATABASES['default'].update(db_from_env)    
+
+
+###
+# tensorflow の FutureWarningがうるさいので。
+###
+import warnings
+warnings.simplefilter('ignore')
