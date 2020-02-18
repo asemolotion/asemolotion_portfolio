64fcc6e9f4280e9dfd84f5035649f6c8207fcf52
広石財 <hiroishizai@takaras-mac.local>
2020-02-18 21:57:07 +0900

    おうむ返し #19

---
+++ line_chatbot/conf_vars.py
@@ -0,0 +1,15 @@
+
+import socket 
+
+hostname = socket.gethostname()
+if 'local' in hostname:
+    from conf.local_settings import LINE_REPLY_ENDPOINT as REPLY_ENDPOINT
+    from conf.local_settings import LINE_CHANNEL_ACCESS_TOKEN as CHANNEL_ACCESS_TOKEN
+    
+    REPLY_ENDPOINT = REPLY_ENDPOINT
+    CHANNEL_ACCESS_TOKEN = CHANNEL_ACCESS_TOKEN
+
+else:
+    import os
+    REPLY_ENDPOINT = os.environ.get('LINE_REPLY_ENDPOINT')
+    CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
--- line_chatbot/urls.py
+++ line_chatbot/urls.py
@@ -5,6 +5,7 @@ app_name = 'line_chatbot'
 # /line_chatbot/
 
 urlpatterns = [
-    path('', TopView.as_view(), name='top')
+    path('', TopView.as_view(), name='top'),
+    path('callback/', callback, name='callback'),  # LINEからのWebhookコールバック
 
 ]
