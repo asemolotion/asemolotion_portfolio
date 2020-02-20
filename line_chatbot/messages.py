1176856576a744d9f2a32bb655e4281a90e04c3c
広石財 <hiroishizai@takaras-mac.local>
2020-02-20 13:45:07 +0900

    bus message update #32

--- line_chatbot/messages.py
+++ line_chatbot/messages.py
@@ -21,7 +21,38 @@ def bus(text):
     Returns:
         string: 返信するメッセージ
     """
-    return "バスで帰る"
+    reply_item = {
+  			"type": "template",
+  			"altText": "this is a confirm template",
+  			"template": {
+  				"type": "carousel",
+  				"columns":[
+  					{
+  						"title": "各種コマンド",
+  						"text":"下のリンクをタップしてください。",
+  						"actions": [
+  							{
+  								"type":"uri",
+  								"label":"配信予約済みリスト",
+  								"uri": "https://nakagawa-reminder.herokuapp.com/main/tasks/list/"
+  							},
+  							{
+  								"type": "uri",
+  								"label": "予備",
+  								"uri":"https://nakagawa-reminder.herokuapp.com/"
+  							},
+  							{
+  								"type":"uri",
+  								"label":"管理画面",
+  								"uri": "https://nakagawa-reminder.herokuapp.com/admin/"
+  							}
+  						]
+  					}
+  				]
+  			}
+  		}    
+    
+    return reply_item
 
 
 def estimated_option(text):
@@ -34,3 +65,33 @@ def estimated_option(text):
         string: 返信するメッセージ
     """
     return predict_reply(text)
+
+def instruction(text):
+    """
+    使い方メッセージを返す関数
+
+    Params:
+        text: string: 送られてきた文字列
+    Returns:
+        string: 返信するメッセージ
+    """
+    instruction_message = """
+    
+    これはasemolotionのポートフォリオ用のラインアカウントです。
+    
+    ＊＊＊＊
+    
+    使い方①
+    「バス」、「長崎」、「福岡」、のどれかの文字を含むメッセージを送ったら
+    長崎福岡のバスのスケジュールが返される。
+
+    使い方②
+    「使い方」という文字を送ればこの説明文が返ってくる。
+
+    使い方③
+    その他のメッセージでは自治体アカウントのような項目を予測して返す。
+    ※サーバの具合で推論に時間がかかったり返信がこない場合があります。
+
+    """
+
+    return instruction_message
